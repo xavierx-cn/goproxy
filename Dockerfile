40f0a85735f1b3064c012fb82b0fe8ddcc869043
haishenming <haishenmingx@gmail.com>
2021-08-17 22:49:37 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM golang:1.16 as builder
+
+WORKDIR /goproxy
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o goproxy
+
+FROM alpine:latest
+
+WORKDIR /goproxy
+COPY --from=builder /goproxy .
+EXPOSE 8080
+
+CMD ["./goproxy"]
+
+
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/goproxy/goproxy"
+)
+
+func main() {
+	proxy := &goproxy.Goproxy{
+		GoBinEnv: append(
+			os.Environ(),
+			"GOPROXY=https://goproxy.cn,direct", // 使用 Goproxy.cn 作为上游代理
+			//"GOPRIVATE=git.example.com",         // 解决私有模块的拉取问题（比如可以配置成公司内部的代码源）
+		),
+		ProxiedSUMDBs: []string{
+			"sum.golang.org https://goproxy.cn/sumdb/sum.golang.org", // 代理默认的校验和数据库
+		},
+		Cacher: goproxy.DirCacher("/data/goproxy"),
+	}
+	if err := http.ListenAndServe("0.0.0.0:8080", proxy); err != nil {
+		panic(err)
+
+	}
+}
